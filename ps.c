b38fdb3122663ae5c3fcad12a1d965ad0779ca02
ajalberd <ajalberd8@gmail.com>
2019-04-23 22:28:00 -0400

    added viewing of both sleeping and running processes as a syscall - call by typing ps

---
+++ ps.c
@@ -0,0 +1,11 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+
+int crsp(void);
+int
+main(void){
+    crsp();
+    exit();
+}
--- sysproc.c
+++ sysproc.c
@@ -6,6 +6,50 @@
 #include "memlayout.h"
 #include "mmu.h"
 #include "proc.h"
+#include "spinlock.h"
+struct {
+  struct spinlock lock;
+  struct proc proc[NPROC];
+} ptable;
+
+int
+sys_crsp(void) //prints out the current running and sleeping processes
+{
+  struct proc *p;
+  /*pushcli();
+  pinit();
+  struct cpu *c = mycpu();
+  c->proc = 0;
+  popcli();*/
+  cprintf("name \t pid \t state \n");
+  cprintf("----------------\n");
+  //for(;;){
+    sti();
+    acquire(&ptable.lock); //get the lock
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->state == RUNNABLE){
+        cprintf("process [%s:%d] is RUNNING\n", p->name, p->pid);
+      }
+      else if(p->state == RUNNING){
+        cprintf("process [%s:%d] is RUNNING\n", p->name, p->pid);
+      }
+      else if( p->state == SLEEPING){
+        cprintf("process [%s:%d] is SLEEPING\n", p->name, p->pid);
+      }
+      /*c->proc = p;
+      switchuvm(p);
+      p->state = RUNNING;
+      swtch(&(c->scheduler), p->context);
+      switchkvm();
+      // Process is done running for now.
+      // It should have changed its p->state before coming back.
+      c->proc = 0;*/
+    }
+    release(&ptable.lock);
+  //}
+  return 0;
+  //release(&ptable.lock);
+}
 
 int
 sys_fork(void)
